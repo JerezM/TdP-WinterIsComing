b3c16f1a37da74eb9d4057c700bad9b7ee38c1cc
JerezM <jerez149@gmail.com>
2021-03-31 14:47:52 -0300

    FileStocker structed added

---
+++ src/main/model/FileStocker.java
@@ -0,0 +1,31 @@
+package main.model;
+
+import java.io.File;
+
+import main.service.entry.EntryPersonalized;
+
+public interface FileStocker {
+	
+	/**
+	 * Se encarga de almacenar todos los archivos pasados por parametro.
+	 * @param files Archivos a almacener.
+	 */
+	public void loadFiles(File[] files);
+	
+	/**
+	 * Se encarga de calcular cuales son las 5 palabras mas utilizadas en todos los archivos almacenado,
+	 * junto con su porcentaje de aparicion respectivo.
+	 * 
+	 * @return Un arreglo de entradas<Palabra, Porcentaje> el cual contendra las 5 palabras mas utilizadas.
+	 */
+	public EntryPersonalized<String, Double>[] generalCalculation();
+	
+	/**
+	 * Se encarga de calcular cuales son las 5 palabras mas utilizadas en el archivo seleccionado,
+	 * junto con su porcentaje de aparicion respectivo.
+	 * 
+	 * @param file Archivo que se desea analizar.
+	 * @return Un arreglo de entradas<Palabra, Porcentaje> el cual contendra las 5 palabras mas utilizadas.
+	 */
+	public EntryPersonalized<String, Double>[] particularCalculation(File file);
+}
---
+++ src/main/model/FileStockerImpl.java
@@ -0,0 +1,42 @@
+package main.model;
+
+import java.io.File;
+
+import main.service.entry.EntryPersonalized;
+
+public class FileStockerImpl implements FileStocker {
+
+	private static FileStockerImpl instance;
+	
+	private File[] files;
+	
+	private FileStockerImpl() {
+		
+	}
+	
+	public static FileStocker getInstance() {
+		if( instance == null ) {
+			instance = new FileStockerImpl();
+		}
+		
+		return instance;
+	}
+	
+	@Override
+	public void loadFiles(File[] files) {
+		this.files = files;
+	}
+
+	@Override
+	public EntryPersonalized<String, Double>[] generalCalculation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EntryPersonalized<String, Double>[] particularCalculation(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
